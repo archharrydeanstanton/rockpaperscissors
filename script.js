e620ecf725d5ae0ee4350b27f3eb1090a52314a2
Joe Pill <joepill@gmail.com>
2021-11-01 11:35:01 -0500

    feat: rewrote the rps console app

--- script.js
+++ script.js
@@ -1,63 +1,47 @@
 const options = ['Rock', 'Paper', 'Scissors'];
+const computerSelection = computerPlay();
 let playerScore = 0;
 let computerScore = 0;
-let tieScore = 0;
-
-function randomNumberZeroOneTwo() {
-    return Math.floor(Math.random() * 3)
-}
 
 function computerPlay() {
-    return options[randomNumberZeroOneTwo()]
+    const computerSelection = options[Math.floor(Math.random() * options.length)];
+    return computerSelection;
 }
 
-function titleCase(str) {
-    str = str.toLowerCase().split(' ');
-    for (let i = 0; i < str.length; i++) {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-    }
-    return str.join(' ');
-}
-
-function playSingleRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, paper, or scissors?", "rock");
-    playerSelection = titleCase(playerSelection);
-    computerSelection = titleCase(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt('Rock, Paper, or Scissors?', 'Rock');
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    computerSelection = computerPlay();
 
     if (playerSelection === computerSelection) {
-        let result = "don't win or lose";
-        let winner = "Nobody";
-        let loser = "anybody";
-        let resultMessage = `You ${result}! ${winner} beats ${loser}.`
-        tieScore ++;
-        return playerScore, computerScore, resultMessage;
-    } else if ((computerSelection == 'Rock' && playerSelection == 'Scissors') || (computerSelection == 'Paper' && playerSelection == 'Rock' ) || (computerSelection == 'Scissors' && playerSelection == 'Paper')) {
-        result = "Lose";
-        winner = computerSelection;
-        loser = playerSelection;
-        resultMessage = `You ${result}! ${winner} beats ${loser}.`
-        computerScore ++;
-        return playerScore, computerScore, resultMessage;
-    } else {
-        result = "Win";
-        winner = playerSelection;
-        loser = computerSelection;
-        resultMessage = `You ${result}! ${winner} beats ${loser}.`
+        console.log('Tie game!')
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
         playerScore ++;
-        return playerScore, computerScore, resultMessage;
+        console.log(`You win this round! ${playerSelection} beats ${computerSelection}!`);
+    } else {
+        computerScore ++;
+        console.log(`You lose this round! ${playerSelection} doesn't beat ${computerSelection}!`);
     }
 }
 
 function game() {
-    playSingleRound();
-    playSingleRound();
-    playSingleRound();
-    playSingleRound();
-    playSingleRound();
-    let resultMessage = `You scored ${playerScore}! The computer scored ${computerScore}. There were ${tieScore} tie games.`;
-    playerScore = 0;
+    let round = 0;
+
+    while (round < 5) {
+        playRound();
+        round++;
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`Game over!`);
+        console.log(`You won! In ${round} rounds, you scored ${playerScore}, while the computer scored ${computerScore}!`);
+    } else if (computerScore > playerScore) {
+        console.log(`Game over!`);
+        console.log(`You lose! In ${round} rounds, you scored ${playerScore}, while the computer scored ${computerScore}!`);   
+    }
+
     computerScore = 0;
-    tieScore = 0;
-    alert(resultMessage);
-    return resultMessage;
+    playerScore = 0;
+    round = 0;
 }
