b22af2df6fd15f5af2954ba18cdbcaef81a85acf
Joe Pill <joepill@gmail.com>
2021-11-01 13:38:04 -0500

    feat: adds buttons and a very basic UI to rps app. refactored code as well.

--- script.js
+++ script.js
@@ -1,47 +1,70 @@
 const options = ['Rock', 'Paper', 'Scissors'];
+const optionsDisplay = document.querySelector("#options");
+const resultDisplay = document.querySelector("#result");
+const resultMessage = document.createElement('div')
+const resultScore = document.createElement('div')
+
+
 const computerSelection = computerPlay();
 let playerScore = 0;
 let computerScore = 0;
+let round = 1;
+
 
 function computerPlay() {
     const computerSelection = options[Math.floor(Math.random() * options.length)];
     return computerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Rock, Paper, or Scissors?', 'Rock');
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    computerSelection = computerPlay();
-
-    if (playerSelection === computerSelection) {
-        console.log('Tie game!')
-    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-        playerScore ++;
-        console.log(`You win this round! ${playerSelection} beats ${computerSelection}!`);
-    } else {
-        computerScore ++;
-        console.log(`You lose this round! ${playerSelection} doesn't beat ${computerSelection}!`);
-    }
+const handleClick = (e) => {
+    playerSelection = e.target.innerHTML;
+    playRound(playerSelection, computerSelection);
 }
 
-function game() {
-    let round = 0;
+options.forEach(option => {
+    const button = document.createElement("button");
+    button.innerHTML = option;
+    button.addEventListener("click", handleClick)
+    optionsDisplay.appendChild(button);
+})
 
-    while (round < 5) {
-        playRound();
-        round++;
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+
+    if (round === 5) {
+        endGame();
+        round = 1;
+        return;
     }
 
+    if (playerSelection === computerSelection) {
+        resultMessage.textContent = 'Tie game!';
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        playerScore ++;
+        resultMessage.textContent = `You win this round! You chose ${playerSelection}, which beats ${computerSelection}.`;
+    } else {
+        computerScore ++;
+        resultMessage.textContent = `You lose this round! You chose ${playerSelection}, which doesn't beat ${computerSelection}.`;
+    }
+    resultScore.textContent = `Round ${round} scores: Player = ${playerScore}. Computer = ${computerScore}.`
+    resultDisplay.appendChild(resultMessage);
+    resultDisplay.appendChild(resultScore);
+    round ++;
+}
+
+function endGame() {
     if (playerScore > computerScore) {
-        console.log(`Game over!`);
-        console.log(`You won! In ${round} rounds, you scored ${playerScore}, while the computer scored ${computerScore}!`);
+        resultMessage.textContent = `Game over!`;
+        resultScore.textContent = `You won! In ${round-1} rounds, you scored ${playerScore}, while the computer scored ${computerScore}!`;
     } else if (computerScore > playerScore) {
-        console.log(`Game over!`);
-        console.log(`You lose! In ${round} rounds, you scored ${playerScore}, while the computer scored ${computerScore}!`);   
+        resultMessage.textContent = `Game over!`;
+        resultScore.textContent = `You lose! After ${round-1} rounds, you scored ${playerScore}, while the computer scored ${computerScore}!`;   
     }
+
+    resultDisplay.appendChild(resultMessage);
+    resultDisplay.appendChild(resultScore);
 
     computerScore = 0;
     playerScore = 0;
-    round = 0;
+    round = 1;
 }
